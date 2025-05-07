9914c1faa396f15fcd1a583715de45c800ef80fc
HanzLeo <hanzincoming@icloud.com>
2025-05-07 18:29:23 +0800

    my neovim dotfile in 2025.

---
+++ lua/options.lua
@@ -0,0 +1,12 @@
+require "nvchad.options"
+
+-- add yours here!
+-- 为 markdown 文件设置 conceallevel=1
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.wo.conceallevel = 1
+  end
+})
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
